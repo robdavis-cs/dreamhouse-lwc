6ab8533923870f327ea9a49a8fe4a67753ce6179
drd0013 <drd0013@gmail.com>
2021-06-25 15:04:51 -0700

    NamespaceAccessible

--- dx-utils/create_package_version.sh
+++ dx-utils/create_package_version.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 #create a package version. Usage: create_package_version.sh PACKAGENAME POSTINSTALLURL TAG
 
-if [ $# -lt 1 ]
-then
-    PACKAGENAME="Dreamhouse Extension"
-    POSTINSTALLURL="https://google.com"
-    TAG=`git log -n 1 origin/master --pretty=format:"%H"`
-else
-    PACKAGENAME=$1
-    POSTINSTALLURL=$2
-    TAG=$3
-fi
+PACKAGENAME=$1
+POSTINSTALLURL="https://google.com"
+TAG=`git log -n 1 origin/master --pretty=format:"%H"`
 
 sfdx force:package:version:create --package "$PACKAGENAME" --wait 30 --postinstallurl $POSTINSTALLURL -x -c --tag $TAG
