62801fe98fa6fe840f3e9d6db49bce352b42d7e5
drd0013 <drd0013@gmail.com>
2021-06-25 16:59:22 -0700

    more POC things

--- force-app/extension/main/default/lwc/propertyDetails/propertyDetails.js
+++ force-app/extension/main/default/lwc/propertyDetails/propertyDetails.js
@@ -1,3 +1,21 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
+import PROPERTY_FIELD from '@salesforce/schema/Property_Details__c.Property__c';
+import getBarredDetailName from '@salesforce/apex/PropertyDetailsController.getBarredDetailName';
+import getProperty from '@salesforce/apex/PropertyDetailsController.getProperty';
 
-export default class PropertyDetails extends LightningElement {}
+export default class PropertyDetails extends LightningElement {
+    @api recordId;
+    barredName = '';
+    property;
+    propertyLoaded = false;
+
+    connectedCallback() {
+        getBarredDetailName({ recordId: this.recordId }).then((res) => {
+            this.barredName = res;
+        });
+        getProperty({ recordId: this.recordId }).then((res) => {
+            this.property = res;
+            this.propertyLoaded = true;
+        });
+    }
+}
